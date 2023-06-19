a8e83d25e4470080781bd38d1cc434d2cc261dde
sudheerkube <sudheerkube@gmail.com>
2023-06-19 22:59:52 +0530

    new files

--- shells.sh
+++ shells.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 var1=newus
 echo "$var1"
 var2=test2
-CICD=1
-if [[$CICD -eq 1]]
+CICD=true
+if [[ "$CICD" = true ]]
 then
     echo "CICD is working"
 else
     echo "Build Not working"
-fi
+ fi
